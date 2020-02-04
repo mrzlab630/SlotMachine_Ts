8dc4ca0db06e91770a202c89cc61b2b8296b9fb4
mrZ <mrzlab630@gmail.com>
2020-02-04 18:40:55 +0300

    change url for docs

--- docs/precache-manifest.9d3f31f1010b206d06a149bf74af8976.js
+++ docs/precache-manifest.9d3f31f1010b206d06a149bf74af8976.js
@@ -25,78 +25,78 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "1650a1ad3f38cc4c340fdcac5a577bee",
-    "url": "/static/media/apple.1650a1ad.svg"
+    "url": "static/media/apple.1650a1ad.svg"
   },
   {
     "revision": "0b3877e26f37ede1c8eaf57427196d95",
-    "url": "/static/media/avocado.0b3877e2.svg"
+    "url": "static/media/avocado.0b3877e2.svg"
   },
   {
     "revision": "886be8da8ef76e3a098790692211f474",
-    "url": "/static/media/bananas.886be8da.svg"
+    "url": "static/media/bananas.886be8da.svg"
   },
   {
     "revision": "bded55a1df283d48c07e151a10b98e4a",
-    "url": "/static/media/cherry.bded55a1.svg"
+    "url": "static/media/cherry.bded55a1.svg"
   },
   {
     "revision": "31dd7fafbdcbcfeb0318f853ff098fcd",
-    "url": "/static/media/coconut.31dd7faf.svg"
+    "url": "static/media/coconut.31dd7faf.svg"
   },
   {
     "revision": "0dd83edbec8ddbcd15e2612f08748e4f",
-    "url": "/static/media/fig.0dd83edb.svg"
+    "url": "static/media/fig.0dd83edb.svg"
   },
   {
     "revision": "3c05444cc4bb1028a65a3f83dc565566",
-    "url": "/static/media/grapes.3c05444c.svg"
+    "url": "static/media/grapes.3c05444c.svg"
   },
   {
     "revision": "156c6d2f43273f0456ab3978ae3070af",
-    "url": "/static/media/guava.156c6d2f.svg"
+    "url": "static/media/guava.156c6d2f.svg"
   },
   {
     "revision": "4f414416a5bc00136638657270fdc40c",
-    "url": "/static/media/kiwi.4f414416.svg"
+    "url": "static/media/kiwi.4f414416.svg"
   },
   {
     "revision": "6e5d6773450d10ba36de9bdb48755387",
-    "url": "/static/media/mangosteen.6e5d6773.svg"
+    "url": "static/media/mangosteen.6e5d6773.svg"
   },
   {
     "revision": "410f94647e6151b5608205ed5bd92801",
-    "url": "/static/media/melon.410f9464.svg"
+    "url": "static/media/melon.410f9464.svg"
   },
   {
     "revision": "29436d46782ed1f19c164579d09f96d4",
-    "url": "/static/media/orange.29436d46.svg"
+    "url": "static/media/orange.29436d46.svg"
   },
   {
     "revision": "5fee44d418cdc5162af9cf38e8698907",
-    "url": "/static/media/papaya.5fee44d4.svg"
+    "url": "static/media/papaya.5fee44d4.svg"
   },
   {
     "revision": "697e1277b7e95fea8e79d7a4ce8f2704",
-    "url": "/static/media/peach.697e1277.svg"
+    "url": "static/media/peach.697e1277.svg"
   },
   {
     "revision": "104e514e8502d2a3e3081237e2ab41d8",
-    "url": "/static/media/pear.104e514e.svg"
+    "url": "static/media/pear.104e514e.svg"
   },
   {
     "revision": "2b66c310189c9b03a3f1553d850cfd5b",
-    "url": "/static/media/pineapple.2b66c310.svg"
+    "url": "static/media/pineapple.2b66c310.svg"
   },
   {
     "revision": "ec7752d052cc04b74289fc1609b69c8f",
-    "url": "/static/media/pitaya.ec7752d0.svg"
+    "url": "static/media/pitaya.ec7752d0.svg"
   },
   {
     "revision": "0457e7cbd396f1ae596afb7adee8ec9e",
-    "url": "/static/media/strawberry.0457e7cb.svg"
+    "url": "static/media/strawberry.0457e7cb.svg"
   },
   {
     "revision": "16f5b0d624d77623c622b8e053a0a970",
-    "url": "/static/media/watermelon.16f5b0d6.svg"
+    "url": "static/media/watermelon.16f5b0d6.svg"
   }
 ]);
